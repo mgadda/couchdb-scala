469bd95c2a1443d5c03c9944dccd84f807dc0cf0
Matt Gadda <mgadda@gmail.com>
2014-01-04 17:43:05 -0800

    clean up

--- src/main/scala/com/github/mgadda/couchdb_scala/Main.scala
+++ src/main/scala/com/github/mgadda/couchdb_scala/Main.scala
@@ -2,18 +2,8 @@ package com.github.mgadda.couchdb_scala
 
 import scala.util.parsing.json.JSON
 import com.github.mgadda.couchdb_scala.util.{JSON => DeepJSON}
-import scala.collection.mutable.{Map => MutableMap}
 
 object Main {
-  // Totally unnecessary because List.map already converts
-  // List[Tuple2[K, V]] to Map[K, V]
-//  implicit def tupleListToMap[K, V](tupleList: List[Tuple2[K, V]]): Map[K, V] = {
-//    tupleList.foldLeft[MutableMap[K, V]](MutableMap[K, V]()) { case (s, (k, v)) =>
-//      s(k) = v
-//      s
-//    }.toMap
-//  }
-
   def executeCmd(cmd: String): Any = {
     JSON.parseFull(cmd) match {
       case Some(List("add_fun", fun: String)) =>
--- src/main/scala/com/github/mgadda/couchdb_scala/ViewServer.scala
+++ src/main/scala/com/github/mgadda/couchdb_scala/ViewServer.scala
@@ -12,7 +12,7 @@ object ViewServer {
     "true"
   }
 
-  // val cmd = "[\"add_fun\", \"def map(doc: Map[String, Any]) { if (doc(\\\"score\\\").asInstanceOf[Double] > 50) { List((None, Map(\\\"player_name\\\" -> doc(\\\"name\\\")))) } }\"]"
+  // Example:
   // ["add_fun", "(doc: Map[String, Any]) => { if (doc(\"score\").asInstanceOf[Double] > 50) { List((None, Map(\"player_name\" -> doc(\"name\")))) } }"]
   def add_fun(fun: String): Either[Map[String, String], String] = {
     val evaler = new Eval()
@@ -26,6 +26,7 @@ object ViewServer {
     }
   }
 
+  // Example:
   // ["map_doc", {"_id":"8877AFF9789988EE","_rev":"3-235256484","name":"John Smith","score": 60}]
   def map_doc(doc: Map[String, Any]): List[List[Any]] = {
     //When the view function is stored in the view server, CouchDB starts sending in all the documents in the database, one at a time. The view server calls the previously stored functions one after another with the document and stores its result. When all functions have been called, the result is returned as a JSON string.
