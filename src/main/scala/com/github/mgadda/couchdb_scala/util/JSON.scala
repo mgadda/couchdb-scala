760fe9faff4de4199b46f8b869bf8c312acb4ec5
Matt Gadda <mgadda@gmail.com>
2014-01-04 17:38:36 -0800

    add support for tuple and None JSON serialization

--- src/main/scala/com/github/mgadda/couchdb_scala/util/JSON.scala
+++ src/main/scala/com/github/mgadda/couchdb_scala/util/JSON.scala
@@ -1,26 +1,39 @@
 package com.github.mgadda.couchdb_scala.util
 
 object JSON {
-  def apply(obj: Any): JSON = obj match {
-    case list: List[Any] => JSON(list)
-    case map: Map[String, Any] => JSON(map)
-    case num: Double => JSON(num)
-    case num: Int => JSON(num)
-    case str: String => JSON(str)
-    case _ => JSON(obj.asInstanceOf[String])
+  def apply(anything: Any): JSON = {
+    anything match {
+      case map: Map[String, _] => new JSONObject(map)
+      case list: List[_] => new JSONArray(list)
+      case num: Double => new JSONDouble(num)
+      case num: Int => new JSONInt(num)
+      case str: String => new JSONString(str)
+      case tuple2: Tuple2[_,_] =>
+        new JSONArray(tuple2.productIterator.toList)
+      case tuple3: Tuple3[_,_,_] =>
+        new JSONArray(tuple3.productIterator.toList)
+      case tuple4: Tuple4[_,_,_,_] =>
+        new JSONArray(tuple4.productIterator.toList)
+      case tuple5: Tuple5[_,_,_,_,_] =>
+        new JSONArray(tuple5.productIterator.toList)
+      case tuple6: Tuple6[_,_,_,_,_,_] =>
+        new JSONArray(tuple6.productIterator.toList)
+      case tuple7: Tuple7[_,_,_,_,_,_,_] =>
+        new JSONArray(tuple7.productIterator.toList)
+      case tuple8: Tuple8[_,_,_,_,_,_,_,_] =>
+        new JSONArray(tuple8.productIterator.toList)
+      case tuple9: Tuple9[_,_,_,_,_,_,_,_,_] =>
+        new JSONArray(tuple9.productIterator.toList)
+      case tuple10: Tuple10[_,_,_,_,_,_,_,_,_,_] =>
+        new JSONArray(tuple10.productIterator.toList)
+      case None => new JSONNull
+    }
   }
-
-  def apply(obj: Map[String, Any]): JSON = new JSONObject(obj)
-  def apply(list: List[Any]): JSON = new JSONArray(list)
-  def apply(str: String): JSON = new JSONString(str)
-  def apply(num: Double): JSON = new JSONDouble(num)
-  def apply(num: Int): JSON = new JSONInt(num)
-
 }
 
 sealed abstract class JSON
 
-class JSONObject(obj: Map[String, Any]) extends JSON {
+class JSONObject(obj: Map[String, _]) extends JSON {
   override def toString = {
     val keyVals = obj.map {
       case(key, value) => s""""$key": ${JSON(value).toString}"""
@@ -46,4 +59,8 @@ class JSONDouble(num: Double) extends JSON {
 
 class JSONInt(num: Int) extends JSON {
   override def toString = num.toString
+}
+
+class JSONNull extends JSON {
+  override def toString = "null"
 }
